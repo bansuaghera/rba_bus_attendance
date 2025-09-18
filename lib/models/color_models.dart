6dce928dc3224b92736726843e526c18b9171125
Bansi aghera <122193589+bansuaghera@users.noreply.github.com>
2025-09-18 15:07:58 +0530

    color model

---
+++ lib/models/color_models.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// A class to hold the application's color palette.
+/// This prevents code duplication and makes it easy to manage colors.
+class AppColors {
+  // Main Colors
+  static const Color red = Color(0xFFF44336);
+  static const Color black = Color(0xFF000000);
+  static const Color grey = Color(0xFF9E9E9E);
+  static const Color green = Color(0xFF4CAF50);
+  static const Color blue = Color.fromARGB(255, 3, 89, 159);
+
+  // Light Shades
+  static const Color lightGreen = Color(0xFF8BC34A);
+  static const Color lightRed = Color(0xFFE57373); // A lighter shade of red
+}
